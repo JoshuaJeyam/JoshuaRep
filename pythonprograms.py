85e6c3923d76e387fa632df872c7a2d61b4bcce7
JoshuaJeyam <144205121+JoshuaJeyam@users.noreply.github.com>
2023-09-06 11:19:26 +0530

    Add files via upload

---
+++ pythonprograms.py
@@ -0,0 +1,13 @@
+#prime number or not.....
+##n=int(input("Enter the number:"))
+##if(n%2!=0):
+##    print("it is a prime number")
+##else:
+##   print( "it is not a prime number")
+
+
+def fibonacci(N):
+    if N==0:
+        return 0
+    else :
+        return 1
